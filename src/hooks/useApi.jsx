a82c5b8c3d95464bea8faa21a5e9941367d11729
unknown <hescu6@gmail.com>
2021-12-28 12:17:25 -0500

    added useApi hook to fetch data from zillow api

---
+++ src/hooks/useApi.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export const useApi = (APIparams) => {
+ const [response, setResponse]=useState(undefined);
+ const [error, setError] =useState('');
+
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
