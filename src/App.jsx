eb4ca4008aa63232ff3f224ca970a7e7cfaa8034
unknown <hescu6@gmail.com>
2021-12-28 14:08:37 -0500

    Logic to fetch API done - TODO:fetch zillow data from my server then respond to app (CORS policy blocking)

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,39 @@ import { useState, useEffect } from "react";
 import HouseCardGrid from "./components/HouseCardGrid";
 import SearchBar from "./components/SearchBar";
 
+import { useApi } from "./hooks/useApi";
+
 import { sampleData } from "./assets/sampleData"; //sample return data from API
 
 function App() {
   const [houses, setHouses] = useState({});
   const [searchQuery, setSearchQuery] = useState();
 
+// //get house listings from Zillow API
+//   const params = getApiParams(searchQuery);
+//   const { response, error, loading } = useApi(params);
+
+  function getApiParams(location) {
+    return {
+      method: "GET",
+      url: "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch",
+      params: { location: location, home_type: "Houses" },
+      headers: {
+        "x-rapidapi-host": "zillow-com1.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_ZILLOW_RAPID_API_KEY,
+      },
+    };
+  }
+
   useEffect(() => {
-    //sub to fetch data from API
-    //fetch from sample data for now (development)
+    //sub to fetch data from API / fetch from hl6server (CORs policy blocking from browser)
+    // if (response) {
+    //   setHouses(response);
+    // }
+
+    //sub fetch from sample data for now (development)
     setHouses(sampleData);
+
   }, [searchQuery]);
 
   return (
--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export const useApi = (APIparams) => {
- const [response, setResponse]=useState(undefined);
- const [error, setError] =useState('');
- const [loading, setloading] = useState(true);
+export function useApi (paramsAPI)  {
+  const [response, setResponse] = useState(undefined);
+  const [error, setError] = useState("");
+  const [loading, setloading] = useState(true);
 
- async function fetchData (params){
-     try {
-         const result = await axios.request(params);
-     } catch (error) {
-         setError(error);
-     } finally {
-setloading(false);
-     }
- }    
+  async function fetchData(params) {
+    try {
+      const result = await axios.request(params);
+      setResponse(result.data)
+    } catch (error) {
+      setError(error);
+    } finally {
+      setloading(false);
+    }
+  }
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  useEffect(() => {
+    fetchData(paramsAPI);
+  }, []);
+
+  return { response, error, loading }
+};
