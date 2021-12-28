62f42c909f24c8355f7887626345fc2aae63db44
unknown <hescu6@gmail.com>
2021-12-28 00:54:23 -0500

    added searchQuery and housing states

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import HouseCardGrid from "./components/HouseCardGrid";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const [houses, setHouses] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <>
       <div className="flex-initial overflow-auto flex-wrap items-center justify-center  h-screen w-screen p-8 bg-gray-800">
         <div>
-          <SearchBar />
+          <SearchBar
+            setSearchQuery={setSearchQuery}
+          />
+
+          <h1>{searchQuery}</h1>
         </div>
-        <HouseCardGrid />
+        <HouseCardGrid houses={houses} />
       </div>
     </>
   );
