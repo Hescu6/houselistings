bb5d390d334f0df3180dbb701e4e496b92dce54e
unknown <hescu6@gmail.com>
2021-12-28 12:01:33 -0500

    added span tooltip to image and button

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -30,9 +30,11 @@ export default function HouseCard({ house }) {
 
   return (
     <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-      <a href={googleMapURL} target="_blank">
-        <img className="rounded-t-lg" src={imgSrc} alt="" />
-      </a>
+      <span title="Open Google Maps">
+        <a href={googleMapURL} target="_blank">
+          <img className="rounded-t-lg" src={imgSrc} alt="" />
+        </a>
+      </span>
       <div className="p-5">
         <a href="#">
           <h3 className="text-gray-900 font-bold text-xl tracking-tighter  dark:text-white">
@@ -49,22 +51,24 @@ export default function HouseCard({ house }) {
           Home features {bedrooms} bedrooms, and {bathrooms} bathroom
           {bathrooms > 1 ? "s" : ""}
         </p>
-        <a
-          href={zillowURL}
-          target="_blank"
-          className="text-white bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
-          Read more
-          <svg
-            className="-mr-1 ml-2 h-4 w-4"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"></path>
-          </svg>
-        </a>
+        <span title="Open Zillow">
+          <a
+            href={zillowURL}
+            target="_blank"
+            className="text-white bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+            Read more
+            <svg
+              className="-mr-1 ml-2 h-4 w-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"></path>
+            </svg>
+          </a>
+        </span>
       </div>
     </div>
   );
