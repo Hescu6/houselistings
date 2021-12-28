093a3fc0b5f228033bd16530ce5444ff74b1bf13
unknown <hescu6@gmail.com>
2021-12-28 11:31:19 -0500

    added key for .map() and fixed props structure

--- src/components/HouseCardGrid.jsx
+++ src/components/HouseCardGrid.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import HouseCard from "./HouseCard";
 
-export default function HouseCardGrid() {
+export default function HouseCardGrid({ houses }) {
+  const { props } = houses;
   return (
     <div className="px-2">
       <div className="flex flex-wrap items-center justify-center -mx-2">
-        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
-          <HouseCard />
-        </div>
-        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
-          <HouseCard />
-        </div>
-        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
-          <HouseCard />
-        </div>
-        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
-          <HouseCard />
-        </div>
-        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
-          <HouseCard />
-        </div>
+        {props.map((house) => (
+          <div
+            className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2"
+            key={house.zpid}>
+            <HouseCard house={house} />
+          </div>
+        ))}
       </div>
     </div>
   );
