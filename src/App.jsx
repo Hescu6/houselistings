9f7d5ddb3897f308287c46a2f7bb68d408a1a97d
unknown <hescu6@gmail.com>
2021-12-28 11:34:33 -0500

    set flex options for searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,57 @@ import { useState, useEffect } from "react";
 import HouseCardGrid from "./components/HouseCardGrid";
 import SearchBar from "./components/SearchBar";
 
+import { sampleData } from "./assets/sampleData"; //sample return data from API
+
 function App() {
-  const [houses, setHouses] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [houses, setHouses] = useState({});
+  const [searchQuery, setSearchQuery] = useState();
+
+  useEffect(() => {
+    /*
+    *********** Data Schema **********
+    {
+      sampleData{
+        props: [
+          {
+            address: string,
+            imgSrc: string,
+            bedrooms: int,
+            bathrooms: int,
+            price: int,
+            livingArea: int,
+            lotAreaUnit: string,
+            lotAreaValue: int,
+            propertyType: string
+            ...
+          },
+          ...
+        ],
+        resultsPerPage: int,
+        totalResultCount: int,
+        totalPages: int,
+      }
+    }
+    
+    */
+    //sub to fetch data from API
+    //fetch from sample data for now (development)
+    setHouses(sampleData);
+    // console.log(houses);
+  }, [searchQuery]);
+
+  // useEffect(()=>{
+  //   console.log(houses)
+
+  // },[houses])
 
   return (
     <>
-      <div className="flex-initial overflow-auto flex-wrap items-center justify-center  h-screen w-screen p-8 bg-gray-800">
+      <div className={`flex${searchQuery ? "initial":""} overflow-auto flex-wrap items-center justify-center  h-screen w-screen p-8 bg-gray-800`}>
         <div>
-          <SearchBar
-            setSearchQuery={setSearchQuery}
-          />
-
-          <h1>{searchQuery}</h1>
+          <SearchBar setSearchQuery={setSearchQuery} />
         </div>
-        <HouseCardGrid houses={houses} />
+        {searchQuery && <HouseCardGrid houses={houses} />}
       </div>
     </>
   );
