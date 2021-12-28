fb978159d1c20ca76a4c0d47b2c4669a28418465
unknown <hescu6@gmail.com>
2021-12-27 21:43:33 -0500

    refractor Houses Card Grid as component - TODO render sample data

---
+++ src/components/HouseCardGrid.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import HouseCard from "./HouseCard";
+
+export default function HouseCardGrid() {
+  return (
+    <div className="px-2">
+      <div className="flex flex-wrap items-center justify-center -mx-2">
+        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
+          <HouseCard />
+        </div>
+        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
+          <HouseCard />
+        </div>
+        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
+          <HouseCard />
+        </div>
+        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
+          <HouseCard />
+        </div>
+        <div className="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 xs:w-1/1 p-2">
+          <HouseCard />
+        </div>
+      </div>
+    </div>
+  );
+}
