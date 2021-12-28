ee4bf3e360f8cca23cdec1e8511eed9bc0b1af31
unknown <hescu6@gmail.com>
2021-12-28 12:22:18 -0500

    added try/catch to axios request

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -4,8 +4,18 @@ import axios from 'axios';
 export const useApi = (APIparams) => {
  const [response, setResponse]=useState(undefined);
  const [error, setError] =useState('');
+ const [loading, setloading] = useState(true);
 
-    
+ async function fetchData (params){
+     try {
+         const result = await axios.request(params);
+     } catch (error) {
+         setError(error);
+     } finally {
+setloading(false);
+     }
+ }    
+
     return (
         <div>
             
