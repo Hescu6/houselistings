a13f07a5b636d258368d00014fa8a9b5de9a4670
unknown <hescu6@gmail.com>
2021-12-29 09:37:43 -0500

    fetch house listings from  hescu6server -> rapidAPI

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,62 @@ import { useState, useEffect } from "react";
 import HouseCardGrid from "./components/HouseCardGrid";
 import SearchBar from "./components/SearchBar";
 
+import axios from "axios";
+
 import { useApi } from "./hooks/useApi";
 
 import { sampleData } from "./assets/sampleData"; //sample return data from API
 
 function App() {
-  const [houses, setHouses] = useState({});
+  const [houses, setHouses] = useState({props:[]});
   const [searchQuery, setSearchQuery] = useState();
+  const [loading, setloading] = useState(false);
 
-// //get house listings from Zillow API
-//   const params = getApiParams(searchQuery);
-//   const { response, error, loading } = useApi(params);
+  // //get house listings from Zillow API
+  //   const params = getApiParams(searchQuery);
+  //   const { response, error, loading } = useApi(params);
+
+const API_URL = "https://hescu6server.herokuapp.com/";
+const ZILLOW_ENDPOINT = "zillow/";
 
   function getApiParams(location) {
     return {
       method: "GET",
-      url: "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch",
-      params: { location: location, home_type: "Houses" },
-      headers: {
-        "x-rapidapi-host": "zillow-com1.p.rapidapi.com",
-        "x-rapidapi-key": process.env.REACT_APP_ZILLOW_RAPID_API_KEY,
-      },
+      url: `http://localhost:61645/zillow/${location}`,
     };
   }
 
+  async function getData(location) {
+    const res = await axios.get(`http://localhost:52998/zillow/${location}`);
+    return res;
+  }
+
   useEffect(() => {
-    //sub to fetch data from API / fetch from hl6server (CORs policy blocking from browser)
+    // //sub to fetch data from API / fetch from hl6server (CORs policy blocking from browser)
     // if (response) {
     //   setHouses(response);
     // }
 
-    //sub fetch from sample data for now (development)
-    setHouses(sampleData);
+    try {
+      setloading(true);
+      axios
+        .get(`${API_URL}${ZILLOW_ENDPOINT}${searchQuery}`)
+        .then((response) => {
+          const { data } = response;
+          setHouses(JSON.parse(data));
+          console.log("IN TRY");
+          console.log(houses);
+        });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setloading(false);
+    }
 
+    console.log("IN EFFECT")
+    // setHouses(data);
+    // //sub fetch from sample data for now (development)
+    // console.log(sampleData);
   }, [searchQuery]);
 
   return (
@@ -47,7 +70,8 @@ function App() {
         <div>
           <SearchBar setSearchQuery={setSearchQuery} />
         </div>
-        {searchQuery && <HouseCardGrid houses={houses} />}
+        { loading && <h2 className="text-gray-900 font-bold text-xl tracking-tighter  dark:text-white">Loading...</h2>}
+        {searchQuery && !loading && <HouseCardGrid houses={houses} />}
       </div>
     </>
   );
